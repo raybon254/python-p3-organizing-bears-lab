91fb8c0aec1c6962e4b37fe8281a98bd4892848a
Brian <musyimib449@gmail.com>
2025-05-21 03:15:20 +0300

    sql test select operation lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Mr.Chocolate',5,'M','Brown','average',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Rowdy',4,'M','Brown','low',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Tabitha',6,'F','Brown','low',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Sergeant Brown',3,'M','Brown','high',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Melissa',1,'F','Brown','high',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Grinch',4,'M','Brown','high',1);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES('Wendy',3,'F','Brown','average',0);
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES(NULL,3,'M','Brown','average',0);
